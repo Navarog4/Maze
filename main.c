67ca2c5f20c0e2480a4f6aee75240a1b3f8009f0
Navarog4 <faustinlegrand3@gmail.com>
2024-12-10 20:15:56 +0100

    Rework of the maze solving (now working) Minor changes and remove unused features

--- main.c
+++ main.c
@@ -19,14 +19,11 @@ void printTable(int *t, int nbBord, boolean inChar)
                 if (t[i*nbBord+j]==0){
                     printf("%c", 178);
                 }
-                else if (t[i*nbBord+j]==1){
-                    printf("%c", 177);
-                }
                 else if (t[i*nbBord+j]==-1){
                     printf("%c", ' ');
                 }
                 else {
-
+                    printf("%c", 177);
                 }
             }
             else if (!inChar)
@@ -36,17 +33,19 @@ void printTable(int *t, int nbBord, boolean inChar)
     }
 }
 
-// Setup blank table
-void setupBlankTable(int *t, int nbBord)
+// Setup random table
+void setupRandomTable(int *t, int nbBord)
 {
-    // Set blank table, with walls
+    // Set random cases table, with walls in between
     for (int i = 0; i<nbBord; i++){
             for(int j = 0; j<nbBord; j++){
-                t[i*nbBord+j]=(i*nbBord+j)+100;
+                t[i*nbBord+j]=i*nbBord+j;
                 if (i%2==0 || j%2==0)
                     t[i*nbBord+j]=0;
             }
     }
+
+    printTable(t, nbBord, 0);
 
     // Set the beginning of the maze to 1
     t[nbBord+1]=1; 
@@ -66,7 +65,8 @@ int testTable(int *t, int nbBord)
     return 1;
 }
 
-int creationLab(int *t, int nbBord) /////V�rification : OK
+// Creation of the maze
+int creationLab(int *t, int nbBord)
 {
     int randomValue, randomValueB, temp;
     do{
@@ -158,124 +158,47 @@ int creationLab(int *t, int nbBord) /////V�rification : OK
 
 // This function calculate the distance from the end of the maze to each case. 
 // These calculations are stored in the tMove table. 
+int calculateFastestPath(int *tMove, int nbBord, int caseNumber)
+{
+
+    // Check if each nearby case is empty, then increment the right case by 1. 
+    if(tMove[caseNumber + 1] < tMove[caseNumber] && tMove[caseNumber + 1] > 0){
+        tMove[caseNumber] = -1;
+        calculateFastestPath(tMove, nbBord, caseNumber + 1);
+    }
+    else if(tMove[caseNumber - 1] < tMove[caseNumber] && tMove[caseNumber - 1] > 0){
+        tMove[caseNumber] = -1;
+        calculateFastestPath(tMove, nbBord, caseNumber - 1);
+    }
+    else if(tMove[caseNumber + nbBord] < tMove[caseNumber] && tMove[caseNumber + nbBord] > 0){
+        tMove[caseNumber] = -1;
+        calculateFastestPath(tMove, nbBord, caseNumber + nbBord);
+    }
+    else if(tMove[caseNumber - nbBord] < tMove[caseNumber] && tMove[caseNumber + 1] > 0){
+        tMove[caseNumber] = -1;
+        calculateFastestPath(tMove, nbBord, caseNumber - nbBord);
+    }
+}
+
+
 int calculateReverseTable(int *tMove, int nbBord, int caseNumber)
 {
     // Check if each nearby case is empty, then increment the right case by 1. 
-    if(tMove[caseNumber + 1] == 1){
+    if(tMove[caseNumber + 1] > tMove[caseNumber] || tMove[caseNumber + 1] == 1){
         tMove[caseNumber + 1] = tMove[caseNumber] + 1;
         calculateReverseTable(tMove, nbBord, caseNumber + 1);
     }
-    else if(tMove[caseNumber - 1] == 1){
+    if(tMove[caseNumber - 1] > tMove[caseNumber] || tMove[caseNumber - 1] == 1){
         tMove[caseNumber - 1] = tMove[caseNumber] + 1;
         calculateReverseTable(tMove, nbBord, caseNumber - 1);
     }
-    else if(tMove[caseNumber + nbBord] == 1){
+    if(tMove[caseNumber + nbBord] > tMove[caseNumber] || tMove[caseNumber + nbBord] == 1){
         tMove[caseNumber + nbBord] = tMove[caseNumber] + 1;
         calculateReverseTable(tMove, nbBord, caseNumber + nbBord);
     }
-    else if(tMove[caseNumber - nbBord] == 1){
+    if(tMove[caseNumber - nbBord] > tMove[caseNumber] || tMove[caseNumber - nbBord] == 1){
         tMove[caseNumber - nbBord] = tMove[caseNumber] + 1;
         calculateReverseTable(tMove, nbBord, caseNumber - nbBord);
-    }
-}
-
-
-void randomResolTab(int *tResol, int nbBord, int origine, int randomN, int randomBegin){
-    int tempVal = tResol[origine];
-    int tempCase = origine;
-
-    tResol[tempCase]=-1;
-    if(randomN != randomBegin){
-        if(randomN == -1)
-            randomN = randomBegin;
-
-        tResol[tempCase]=-1;
-        if(randomN == 1){
-            if(tResol[tempCase+1]<tempVal && tResol[tempCase+1]!=0 && tResol[tempCase+1]!=-1){
-                tempVal=tResol[tempCase+1];
-                tResol[tempCase]=-1;
-                tempCase++;
-            }
-            else{
-                randomResolTab(tResol, nbBord, origine, (randomN + 1)%4, randomBegin);
-            }
-        }
-        else if (randomN == 2){
-            if(tResol[tempCase-1]<tempVal && tResol[tempCase-1]!=0 && tResol[tempCase-1]!=-1){
-                tempVal=tResol[tempCase-1];
-                tResol[tempCase]=-1;
-                tempCase--;
-            }
-            else{
-                randomResolTab(tResol, nbBord, origine, (randomN + 1)%4, randomBegin);
-            }
-        }
-        else if (randomN == 3){
-            if(tResol[tempCase-nbBord]<tempVal && tResol[tempCase-nbBord]!=0 && tResol[tempCase-nbBord]!=-1){
-                tempVal=tResol[tempCase-nbBord];
-                tResol[tempCase]=-1;
-                tempCase-=nbBord;
-            }
-            else{
-                randomResolTab(tResol, nbBord, origine, (randomN + 1)%4, randomBegin);
-            }
-        }
-        else if (randomN == 0){
-            if(tResol[tempCase+nbBord]<tempVal && tResol[tempCase+nbBord]!=0 && tResol[tempCase+nbBord]!=-1){
-                tempVal=tResol[tempCase+nbBord];
-                tResol[tempCase]=-1;
-                tempCase+=nbBord;
-            }
-            else{
-                randomResolTab(tResol, nbBord, origine, (randomN + 1)%4, randomBegin);
-            }
-        }
-    }
-}
-
-int resolTab(int *tResol, int nbBord, int origine, int fin){
-    int tempVal = tResol[origine];
-    int tempCase = origine;
-    int randomBegin;
-    do{ 
-        randomBegin = rand()%4;
-
-        for (int i = 0; i < 100; i++)
-        {
-            randomResolTab(tResol, nbBord, origine, -1, randomBegin);
-        }
-        
-        for (int i = 0; i<nbBord*nbBord; i++){
-            if(tResol[i]!=0 && tResol[i]!=-1)
-                tResol[i]=1;
-        }
-        
-        tResol[tempCase]=-1;
-        if(tResol[tempCase+1]<tempVal && tResol[tempCase+1]!=0 && tResol[tempCase+1]!=-1){
-            tempVal=tResol[tempCase+1];
-            tResol[tempCase]=-1;
-            tempCase++;
-        }
-        else if(tResol[tempCase-1]<tempVal && tResol[tempCase-1]!=0 && tResol[tempCase-1]!=-1){
-            tempVal=tResol[tempCase-1];
-            tResol[tempCase]=-1;
-            tempCase--;
-        }
-        else if(tResol[tempCase-nbBord]<tempVal && tResol[tempCase-nbBord]!=0 && tResol[tempCase-nbBord]!=-1){
-            tempVal=tResol[tempCase-nbBord];
-            tResol[tempCase]=-1;
-            tempCase-=nbBord;
-        }
-        else if(tResol[tempCase+nbBord]<tempVal && tResol[tempCase+nbBord]!=0 && tResol[tempCase+nbBord]!=-1){
-            tempVal=tResol[tempCase+nbBord];
-            tResol[tempCase]=-1;
-            tempCase+=nbBord;
-        }
-        printTable(tResol, nbBord, 0);
-    }while (tResol[fin]==1);
-    for (int i = 0; i<nbBord*nbBord; i++){
-        if(tResol[i]!=0 && tResol[i]!=-1)
-            tResol[i]=1;
     }
 }
 
@@ -285,81 +208,39 @@ int main(void)
     int nbBord;
     int point;
 
-    //-----------------------------------------------Initialisaytion du labyrinthe-----------------------------------------------------------//
+    // Maze initialization
     printf("\nHow much for the maze size ? ");
     scanf("%i", &nbBord);
     if (nbBord%2==0)
         nbBord++;
 
-    t = calloc(nbBord*nbBord, sizeof(int)); ////Assignement de la m�moire de stockage du labyrinthe
+    t = calloc(nbBord*nbBord, sizeof(int));
     tMove = calloc(nbBord*nbBord, sizeof(int));
     tResol = calloc(nbBord*nbBord, sizeof(int));
 
     setupBlankTable(&t[0], nbBord);
 
-    //-----------------------------------------------Cr�ation du labyrinthe int�rieur--------------------------------------------------------//
+    // Maze setup
     creationLab(&t[0], nbBord);
 
     printTable(t, nbBord, 1);
 
-    //-----------------------------------------------Partie r�solution du labyrinthe--------------------------------------------------------//
+    // Maze resolution
     for (int i=0; i<nbBord*nbBord; i++){
         tMove[i] = t[i];
     }
 
-    tMove[nbBord*nbBord-nbBord]=2;
+    tMove[nbBord * nbBord - nbBord - 1]=2;
     calculateReverseTable(tMove, nbBord, nbBord*nbBord-1-nbBord);
 
-    printTable(tMove, nbBord, 0);
-    system("pause");
-
-    //-----------------------------------------------Partie r�solution du labyrinthe le plus simplement-------------------------------------//
     for (int i=0; i<nbBord*nbBord; i++){
         tResol[i] = tMove[i];
     }
 
-
-    system("cls");
-
     printf("\nLe labyrinthe est : \n\n");
-    printTable(t, nbBord, 1);
-
-    /*
-    printf("\nVeux-tu placer un point a atteindre avant l'arrivee ? (1/0) ");
-    scanf("%i", &point);*/
-    if (point!=1){
-        printf("\nSi vous continuez, vous obtiendrez la resolution du labyrinthe.");
-
-        printf("\nLa resolution la plus simple du labyrinthe est : \n");
-        resolTab(&tResol[0], nbBord, nbBord, nbBord*nbBord-1-nbBord);
-    }/*
-    else
-    {
-        for (int i=0; i<nbBord*nbBord; i++){
-            tMove[i] = t[i];
-        }
-        do{
-            printf("\nQuel point ? (entre 0 et %i ", nbBord*nbBord-1);
-            scanf("%i", &pointPos);
-            if(t[pointPos]!=1){
-                printf("\nY'a un truc dessus...\n");
-            }
-        }while(t[pointPos]!=1);
-        if (t[pointPos+1]==1)
-            t[pointPos+1]=2;
-        else if (t[pointPos-1]==1)
-            t[pointPos-1]=2;
-        else if (t[pointPos-nbBord]==1)
-            t[pointPos-nbBord]=2;
-        else if (t[pointPos+nbBord]==1)
-            t[pointPos+nbBord]=2;
-        int var=2;
-        for (int i=0; i < nbBord*nbBord; i++){
-            (&tMove[0], var, nbBord);
-            var++;
-        }
-        resolTab(&tResol[0], nbBord, nbBord,pointPos);
-    }*/
+    calculateFastestPath(tMove, nbBord, nbBord);
+    tMove[nbBord*nbBord-1-nbBord] = -1;
+    printTable(tMove, nbBord, 1);
 
     free(tMove);
     free(t);
